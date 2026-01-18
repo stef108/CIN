243dfd6145fb59ac28af31d10525938eef79796c
Stef de Meulmeester <meul0086@hz.nl>
2026-01-18 20:45:32 +0100

    fix: add missing log import

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 	"sync"
@@ -107,7 +108,7 @@ func DeleteHandler(w http.ResponseWriter, r *http.Request) {
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	  if _, err := w.Write([]byte("OK")); err != nil {
-        log.Printf("failed to write response: %v", err)
-    }
+	if _, err := w.Write([]byte("OK")); err != nil {
+		log.Printf("failed to write response: %v", err)
+	}
 }
